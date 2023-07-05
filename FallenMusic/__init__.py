20b3474bcd15fb2a82e1f83e7ee166d6fb104794
AADI-001 <131204586+AADI-001@users.noreply.github.com>
2023-07-05 22:46:57 +0530

    Update __init__.py

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,8 +92,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("DevilsHeavenMF")
-        await app2.join_chat("FallenAssociation")
+        await app2.join_chat("fun_with_friendz")
+        await app2.join_chat("narcotic_world")
     except:
         pass
 
